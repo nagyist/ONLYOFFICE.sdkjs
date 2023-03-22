943ba86807df4558f7d4306cea041d2455a63e83
Kulikova Svetlana <Svetlana.Kulikova@onlyoffice.com>
2023-03-22 13:46:29 +0300

    fix

--- pdf/src/engine/drawingfile.js
+++ pdf/src/engine/drawingfile.js
@@ -606,7 +606,7 @@ else
 			let np1 = reader.readInt();
 			let np2 = reader.readInt();
 			// Указатель на память, аналогичный возвращаемому getPagePixmap. Память необходимо освободить
-			rec["AP"]["retValue"] = rnp2 << 32 | np1;
+			rec["AP"]["retValue"] = np2 << 32 | np1;
 			
 			rec["alignment"] = reader.readInt();
 			rec["type"] = reader.readString();
