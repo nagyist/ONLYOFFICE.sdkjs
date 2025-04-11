0dba6814485d7eb072a92fe0c51639b42c5ba1ba
Eduard Belozertsev <eduard.belozertsev@onlyoffice.com>
2025-04-11 15:39:01 +0700

    [toggle-chart-elements] Replace event ("asc_onSingleChartSelection" -> "asc_onSingleChartSelectionChanged")

--- common/Charts/DrawingObjects.js
+++ common/Charts/DrawingObjects.js
@@ -2083,30 +2083,29 @@ CSparklineView.prototype.setMinMaxValAx = function(minVal, maxVal, oSparklineGro
 
 	_this.checkChartSelection = function () {
 		const ws = _this.getWorksheet();
-		if (!ws) {
-			return;
+		if (ws) {
+
+			const selectedArray = Asc.editor.getGraphicController().getSelectedArray();
+			if (selectedArray.length !== 1) {
+
+				const selectedObject = selectedArray[0];
+				if (selectedObject.isChart && selectedObject.isChart()) {
+
+					const ppi = ws._getPPIX();
+					const mmToPx = Asc.getCvtRatio(3, 0, ppi);
+
+					const bounds = selectedObject.getRectBounds();
+					const l = AscCommon.AscBrowser.convertToRetinaValue(bounds.l * mmToPx - ws._getOffsetX() + ws.cellsLeft);
+					const t = AscCommon.AscBrowser.convertToRetinaValue(bounds.t * mmToPx - ws._getOffsetY() + ws.cellsTop);
+					const r = AscCommon.AscBrowser.convertToRetinaValue(bounds.r * mmToPx - ws._getOffsetX() + ws.cellsLeft);
+					const b = AscCommon.AscBrowser.convertToRetinaValue(bounds.b * mmToPx - ws._getOffsetY() + ws.cellsTop);
+
+					const chartSpaceRect = new AscCommon.asc_CRect(l, t, r - l, b - t);
+					return Asc.editor.sendEvent('asc_onSingleChartSelectionChanged', chartSpaceRect);
+				}
+			}
 		}
-
-		const selectedArray = Asc.editor.getGraphicController().getSelectedArray();
-		if (selectedArray.length !== 1) {
-			return;
-		}
-
-		const selectedObject = selectedArray[0];
-		if (selectedObject.isChart && selectedObject.isChart()) {
-			const bounds = selectedObject.getRectBounds();
-
-			const ppi = ws._getPPIX();
-			const mmToPx = Asc.getCvtRatio(3, 0, ppi);
-
-			const l = AscCommon.AscBrowser.convertToRetinaValue(bounds.l * mmToPx - ws._getOffsetX() + ws.cellsLeft);
-			const t = AscCommon.AscBrowser.convertToRetinaValue(bounds.t * mmToPx - ws._getOffsetY() + ws.cellsTop);
-			const r = AscCommon.AscBrowser.convertToRetinaValue(bounds.r * mmToPx - ws._getOffsetX() + ws.cellsLeft);
-			const b = AscCommon.AscBrowser.convertToRetinaValue(bounds.b * mmToPx - ws._getOffsetY() + ws.cellsTop);
-
-			const chartSpaceRect = new AscCommon.asc_CRect(l, t, r - l, b - t);
-			Asc.editor.sendEvent('asc_onSingleChartSelection', chartSpaceRect);
-		}
+		return Asc.editor.sendEvent('asc_onSingleChartSelectionChanged', null);
 	};
 
     _this.checkImageBullets = function (currentSheet, arrImages) {
