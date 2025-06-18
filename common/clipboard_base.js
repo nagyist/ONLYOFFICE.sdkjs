3a66d4cbd97bad7b7ad52b9c59ff6262c03d68d7
GoshaZotov <Igor.Zotov@onlyoffice.com>
2025-06-18 11:27:04 +0300

    [all] Fix bug 75321

--- common/clipboard_base.js
+++ common/clipboard_base.js
@@ -1059,8 +1059,10 @@
 					//don't put custom format, because FF don't write all in clipboard, if we try write custom format
 					//"web text/x-custom" : new Blob(["asc_internalData2;" + copy_data.data[c_oAscClipboardDataFormat.Internal]], {type: "web text/x-custom"})
 
-					const data = [new ClipboardItem(clipboardData)];
-					navigator.clipboard.write(data).then(function(){},function(){});
+					if (this.isCopyOutEnabled()) {
+						const data = [new ClipboardItem(clipboardData)];
+						navigator.clipboard.write(data).then(function(){},function(){});
+					}
 
 					if (isCut === true)
 						this.Api.asc_SelectionCut();
