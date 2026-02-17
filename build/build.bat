1768b32a98a1e80c20cc3358ed1e842dfae42365
Ilya Kirillov <ilya.kirillov@onlyoffice.com>
2026-02-18 00:58:12 +0300

    Revert build.bat

--- build/build.bat
+++ build/build.bat
@@ -2,7 +2,6 @@ CD /D %~dp0
 call npm install -g grunt-cli
 call npm ci
 rem call grunt --level=WHITESPACE_ONLY --desktop=false --formatting=PRETTY_PRINT
-rem call grunt --level=ADVANCED 
-call grunt --level=ADVANCED --desktop=false --addon=sdkjs-forms --addon=sdkjs-ooxml
+call grunt --level=ADVANCED
 
 pause
